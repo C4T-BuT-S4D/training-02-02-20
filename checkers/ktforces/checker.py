1b930752feec165cf8b414371a421ebba2ea77d3
Ilya <xmikasax.main@gmail.com>
2020-01-28 15:41:15 +0300

    fix selenium options

--- checkers/ktforces/checker.py
+++ checkers/ktforces/checker.py
@@ -23,6 +23,8 @@ def get_driver():
     chrome_options.add_argument(f'user-agent={rnd_useragent()}')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--no-sandbox')
 
 
     while True:
