68df52b91fecc5bb166db2f1a092aed573bec5a8
Alagunto <priah@mail.ru>
2020-02-02 14:28:36 +0300

    Added a usual word to flagged posts titles

--- checkers/7kek/kek_lib.py
+++ checkers/7kek/kek_lib.py
@@ -64,7 +64,7 @@ class CheckMachine:
         if content is None:
             content = rnd_string(40)
 
-        title = rnd_string(24)
+        title = rnd_string(24) + " lol"
 
         r = sess.post(f'{self.url}/api/posts',
                       data={'title': title, 'description': content, 'type': 'image', 'src': random.choice([
